19263eb7b9abae1770e004a0782d7af0b6611ac7
Krzysztof Kolasa <kkolasa@winsoft.pl>
2015-09-19 11:29:24 +0200

    Rename fpu_save_init() to copy_fpregs_to_fpstate()
    Thanks ecforum, https://lkml.org/lkml/2015/5/5/944

--- firegl_public.c
+++ firegl_public.c
@@ -5994,7 +5994,7 @@ void ATI_API_CALL KCL_fpu_begin(void)
     preempt_disable();
     if (cur_thread->status & TS_USEDFPU)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,2,0)
-        fpu_save_init(&cur_task->thread.fpu);
+        copy_fpregs_to_fpstate(&cur_task->thread.fpu);
 #else
         __save_init_fpu(cur_task);
 #endif
@@ -6017,7 +6017,7 @@ void ATI_API_CALL KCL_fpu_begin(void)
     if (cur_task->thread.has_fpu)
 #endif
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,2,0)
-        fpu_save_init(&cur_task->thread.fpu);
+        copy_fpregs_to_fpstate(&cur_task->thread.fpu);
 #else
         __save_init_fpu(cur_task);
 #endif
